626a79b9ded71f657d2b3eb89aba35d9e0fcba28
Jesús <jrodriguez@axonico.net>
2022-09-20 17:04:07 -0300

    Patron

---
+++ src/componentes/Logo/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from '../../images/logo.png'
+import './index.scss'
+const Logo = ()=>{
+  return <>
+      <img id="logo" src={logo}></img>
+  
+  </>
+}
+
+
+export default Logo
--- src/componentes/Patron/index.jsx
+++ src/componentes/Patron/index.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import Select from 'react-select';
+import Logo from "../Logo";
 import './index.scss'
-
-const select  = document.getElementById('select')
+import Data from '../../datos/patron.json'
+import { useEffect } from "react";
+import { useState } from "react";
 
 const options = [
   { value: 'chocolate', label: 'Chocolate' },
   { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
+  { value: 'vanilla', label: 'Vanilla' },
+  { value: 'strawberry', label: 'Strawberry' }
 ]
 
 
 const Patron = () =>{
-
+  const [patron, setPatron] = useState(Data)
+  const [values, setValues] = useState([])
+  
+  useEffect(()=>{    
+    patron.map((patrones)=>{
+      console.log(patrones)
+      console.log(values)
+      //setValues(...values, {values: patrones.id_sector, label: `Patron ${patrones.id_patron} - ${patrones.patron}`})
+      setValues((preState)=> [...preState, {values: patrones.id_sector, label: `Patron ${patrones.id_patron} - ${patrones.patron}`}])
+        
+       
+    })
+    console.log(values)
+  },[])
   return <>
-        <div id="patron">
-          <Select className="select__patron" options={options}/>
+        <div id="patron">          
+            <Logo/>
+            <Select className="select__patron" options={values}/>          
           </div>
   
   </>
